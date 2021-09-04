f4263ef8511ba33b04610b13d6708499e40399b9
Abhishek Anand <as1965286@gmail.com>
2021-09-04 11:10:15 +0530

    Added Tutorial 1

--- TKinter_1.py
+++ TKinter_1.py
@@ -1,1 +1,26 @@
-print ("Hello World!!")
+from tkinter import *
+from tkinter.ttk import *
+
+
+# We First need to create a main window
+# to put Widgets in in.
+# By the way Widgets are the elements of the GUI application
+# Which provides various controls to users to interact with
+# the application.
+
+
+# Some of the widgets in tkinter are radiobutton, checkbox, labels etc.
+
+
+# Creating root window.
+
+root = Tk()
+
+# Giving title to the main window!!
+
+root.title('First Program.')
+
+# Label is what output will be
+# Show on the window.
+label = Label(root, text = "Hello World!").pack()
+
